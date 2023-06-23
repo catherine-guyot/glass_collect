5695eb3902d215b0fc691fd9954fefd9490bbfc1
adeline balian <ad.balian@laposte.net>
2023-06-23 19:06:15 +0200

    show_deposit

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,5 +1,24 @@
 class DepositsController < ApplicationController
-  def show
 
+  def show
+    @deposit = Deposit.find(params[:id])
   end
+
+  def new
+    @deposit = Deposit.new
+  end
+
+  def create
+    @deposit = Deposit.new(deposit_params)
+    @deposit.save
+    redirect_to deposit_path(@deposit)
+  end
+
+
+  private
+
+  def flat_params
+    params.require(:deposit).permit(:name, :address, :total_capacity, :remaining_capacity, :status, :rating, :longitude, :latitude, :photo)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Deposit.destroy_all
+deposit1 = Deposit.create(id: 2, name: "Big_cat", address: "36 Rue de la Madeleine, 69007 Lyon", total_capacity: 9, remaining_capacity: 5, status: 2, rating: 4, longitude: 2.287592, latitude: 48.862725)
