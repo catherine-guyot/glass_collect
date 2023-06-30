4956a61744167a3fc600a4800d7f034712acca1b
adeline balian <ad.balian@laposte.net>
2023-06-30 13:17:38 +0200

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,42 @@
 class ReviewsController < ApplicationController
 
+  def index
+    @deposit = Deposit.find(params[:deposit_id])
+    @review = Review.new
+    @reviews = Review.all
+    @reviews = @deposit.reviews
 
-  # def show
-  #   @deposit = Deposit.find(params[:id])
-  #   @deposit = Deposit.new
-  # end
+  end
 
-  # def create
-  #   @deposit = Deposit.find(params[:restaurant_id])
-  #   @review = Review.new(review_params)
-  #   @review.deposit = @deposit
-  #   if @review.save
-  #     redirect_to restaurant_path(@deposit)
-  #   else
-  #     render "deposit/show", status: :unprocessable_entity
-  #   end
-  # end
+  def show
+    @deposit = Deposit.find(params[:deposit_id])
+    @review = @deposit.reviews.find(params[:id])
 
-  # private
+  end
 
-  # def review_params
-  #   params.require(:review).permit(:content)
-  # end
+  def create
+    @deposit = Deposit.find(params[:deposit_id])
+    @review = Review.new(review_params)
+    @review.deposit = @deposit
+    @review.user = current_user
+    if @review.save
+      redirect_to deposit_reviews_path(@deposit)
+    else
+      render "reviews/index", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to deposit_reviews_path(@review.deposit), notice: 'Le commentaire a été supprimé avec succès.'
+    # redirect_to root_path, status: :see_other
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :deposits, only: [:create, :index, :show] do
-    resources :bookings, only: [:new, :create, :show, :destroy, :update]
-    resources :reviews, only: [:create, :show, :update, :destroy]
+    resources :bookings
+    resources :reviews
+
   end
 
   get 'dashboard', to: 'pages#dashboard'
 end
-
-# delete "flats/:id", to: "flats#destroy", as: "delete_flat"
