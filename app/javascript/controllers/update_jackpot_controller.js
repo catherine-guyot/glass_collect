c6663ed2610c1b85eca78fb558f843000fa1588c
adeline balian <ad.balian@laposte.net>
2023-07-10 21:28:38 +0200

    pimp

--- app/javascript/controllers/update_jackpot_controller.js
+++ app/javascript/controllers/update_jackpot_controller.js
@@ -5,52 +5,43 @@ import confetti from 'canvas-confetti';
 
 // Connects to data-controller="update-jackpot"
 export default class extends Controller {
-  static targets = ['quantity','total', 'startDate', ]
+  static targets = ['total','quantity']
 
   connect() {
-    this.updateTotal();
-    // console.log(this.quantityTarget)
+    // this.updateTotal();
   }
 
-  reward() {
-    this.updateTotal();
-    // console.log("hi")
+  reward(event) {
+    this.updateTotal(event);
     this.celebrate();
-  }
+    this.textinput(event);
 
-  callcelebrate(){
-    this.celebrate();
   }
 
   celebrate() {
     confetti();
   }
 
+  textinput(event){
+    const quantity = parseInt(event.currentTarget.value,10)
+    this.quantityTarget.innerText = quantity
+  }
 
-  updateTotal() {
-  console.log(this.quantityTarget.value)
-  const quantity = parseInt(this.quantityTarget.value,10);
-
-
-  // console.log(typeof quantity);
-  if (quantity >= 0) {
-  const total = quantity * (1/2)
-
-  // console.log(total)
-  this.totalTarget.innerText = total
-  // console.log(this.totalTarget.innerText)
-
-
-
-
-  } else {
+  updateTotal(event) {
+    // console.log(this.quantityTarget.value);
+    const quantity = parseInt(event.currentTarget.value,10);
+    // console.log(typeof quantity);
+    if (quantity >= 0) {
+      const total = quantity * (1/2)
+      // console.log(total)
+      this.totalTarget.innerText = total
+      // console.log(this.totalTarget.innerText)
+    } else {
       // console.log(startDate)
       // console.log(quantity)
       this.totalTarget.textContent = "0"
     }
-
   }
-
 }
 
 
