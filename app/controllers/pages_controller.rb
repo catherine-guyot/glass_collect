cd7a4b9fde703e02f08a64a4369bcfe3f8c80a1d
catherine guyot <catherineguyot@nouveauxcaps.com>
2023-07-11 11:57:19 +0200

    last_jackpot_show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @bookings = current_user.bookings
+    current_user.booking.number_of_bottles = current_user.booking.number_of_bottles + @booking.number_of_bottles
   end
 
   def about
